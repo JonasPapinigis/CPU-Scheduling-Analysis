fadc7b29393664d0f94d52c44c219cff85519863
JonasPapinigis <111145459+JonasPapinigis@users.noreply.github.com>
2024-04-05 15:40:03 +0100

    FeedbackRR, untested

--- os-coursework1/os-coursework1/src/main/java/FeedbackRRScheduler.java
+++ os-coursework1/os-coursework1/src/main/java/FeedbackRRScheduler.java
@@ -2,34 +2,63 @@ import java.util.*;
 
 /**
  * Feedback Round Robin Scheduler
- * 
+ *
  * @version 2017
  */
 public class FeedbackRRScheduler extends AbstractScheduler {
+  private HashMap<Integer, Queue<Process>> multiLevelQueue;
+  private Integer timeQuantum;
+  /**
+   * Adds a process to the ready queue.
+   * usedFullTimeQuantum is true if process is being moved to ready
+   * after having fully used its time quantum.
+   */
 
-  int timeQuantum;
-  Queue<Process> readyQueue;
 
-  @Override
-  public void initialize(Properties parameters) {
-
+  public FeedbackRRScheduler(){
+    multiLevelQueue = new HashMap<>();
   }
-
   public void ready(Process process, boolean usedFullTimeQuantum) {
+    int priority = process.getPriority();
+    if (usedFullTimeQuantum) {
+      // Increase the priority number (decrease priority level)
+      priority++;
+      process.setPriority(priority);
+    }
 
-    // TODO
+    multiLevelQueue.putIfAbsent(priority, new LinkedList<>());
+    multiLevelQueue.get(priority).add(process);
 
   }
 
   /**
-   * Removes the next process to be run from the ready queue 
-   * and returns it. 
+   * Removes the next process to be run from the ready queue
+   * and returns it.
    * Returns null if there is no process to run.
    */
   public Process schedule() {
 
-    // TODO
+    for (Integer priority : new TreeSet<>(multiLevelQueue.keySet())) {
+      Queue<Process> queue = multiLevelQueue.get(priority);
+      if (!queue.isEmpty()) {
+        System.out.println("Scheduler selects process "+queue.peek());
+        return queue.poll(); // Return and remove the process from the queue
+      }
+    }
 
-    return null;
+    return null; // No process to run
+  }
+
+  public void initialise(Properties p){
+    timeQuantum = Integer.parseInt(p.getProperty("timeQuantum"));
+  }
+
+  @Override
+  public int getTimeQuantum(){
+    return timeQuantum;
+  }
+  @Override
+  public boolean isPreemptive(){
+    return true;
   }
 }
